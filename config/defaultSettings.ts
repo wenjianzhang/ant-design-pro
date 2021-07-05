6eb966f2e557d448865df4224c502fd387cc1dae
zhangwenjian <zwj777@live.com>
2021-07-05 09:49:48 +0800

    🏰 chore(config): 修改配置

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -5,17 +5,16 @@ const Settings: LayoutSettings & {
   logo?: string;
 } = {
   navTheme: 'light',
-  // 拂晓蓝
   primaryColor: '#1890ff',
   layout: 'mix',
   contentWidth: 'Fluid',
-  fixedHeader: false,
+  fixedHeader: true,
   fixSiderbar: true,
-  colorWeak: false,
   title: 'Ant Design Pro',
   pwa: false,
+  headerHeight: 48,
+  splitMenus: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-  iconfontUrl: '',
 };
 
 export default Settings;
